7735149f6a9a70cfc9c432d1b19dba84b3566ba6
ShanuSaini <shanusaini434@gmail.com>
2017-01-16 13:08:36 -0800

    update

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'offlineRocks4';
+var cacheName = 'offlineRocks1';
 
 self.addEventListener("install", function(event){
 	event.waitUntil(
@@ -32,32 +32,55 @@ self.addEventListener("activate", function(event){
 });
 
 self.addEventListener("fetch", function(event){
-	event.respondWith(
-		caches.match(event.request).then(function(response){
-				if(response){
-					console.log("[ServiceWorker] found in cache ", event.request.url);
-					return response;
-				}
-				var requestClone = event.request.clone();
+	// event.respondWith(
+	// 	caches.match(event.request).then(function(response){
+	// 			if(response){
+	// 				console.log("[ServiceWorker] found in cache ", event.request.url);
+	// 				return response;
+	// 			}
+	// 			var requestClone = event.request.clone();
 
-				fetch(requestClone).then(function(response){
-					if(!response){
-						console.log("sv no response from fetch");
-						return response;
-					}
+	// 			fetch(requestClone).then(function(response){
+	// 				if(!response){
+	// 					console.log("sv no response from fetch");
+	// 					return response;
+	// 				}
 
-					var responseClone = response.clone();
+	// 				var responseClone = response.clone();
 
-					caches.open(cacheName).then(function(cache){
-						console.log("New Data New ", event.request.url);
-						cache.put(event.request, responseClone);
-						return response;
-					});
-				})
-				.catch(function(err){
-					console.log("error", err);
-					return response;
-				})
-		})
-	);
+	// 				caches.open(cacheName).then(function(cache){
+	// 					console.log("New Data New ", event.request.url);
+	// 					cache.put(event.request, responseClone);
+	// 					return response;
+	// 				});
+	// 			})
+	// 			.catch(function(err){
+	// 				console.log("error", err);
+	// 				return response;
+	// 			})
+	// 	})
+	// );
+
+	console.log('Handling fetch event for', event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        console.log('Found response in cache:', response);
+
+        return response;
+      }
+      console.log('No response found in cache. About to fetch from network...');
+
+      return fetch(event.request).then(function(response) {
+        console.log('Response from network is:', response);
+
+        return response;
+      }).catch(function(error) {
+        console.error('Fetching failed:', error);
+
+        throw error;
+      });
+    })
+  );
 });
